706e5c1ba63fbb4c620039df32083a12a475832c
Matt Robinson <mattrrobins@gmail.com>
2022-02-17 16:55:22 -0800

    reconfigured the directory structure to allow for easier python testing before including the tex file.

--- neuralNetworks/src/py/logReg/logRegCostFctn.py
+++ neuralNetworks/src/py/logReg/logRegCostFctn.py
@@ -49,6 +49,40 @@ def cost_function(x, y, w, b):
 
     # Compute dw and db
     dw = (x @ dz.T) / m
+    assert dw.shape == w.shape
     db = np.sum(dz) / m
 
     return J, dw, db
+
+def grad_descent(x, y, w, b, alpha=0.001, num_iters=100):
+    """
+    Parameters
+    ----------
+    x, y, w, b : See cost_function above for specifics.
+        w and b are chosen to initialize the descent (likely all components 0)
+    alpha : float
+        The learning rate of gradient descent
+    num_iters : int
+        The number of times we wish to perform gradient descent
+
+    Returns
+    -------
+    J_list : List[float]
+        For each iteration we record the cost-values associated to (w, b)
+    w_opt : array_like
+        w_opt.shape = w.shape
+        The optimized parameter w found after performing the descent iterations
+    b_opt : float
+        The optimized paramter b found after performing the descent iterations
+    """
+
+    J_list = []
+    w_opt = w.copy()
+    b_opt = b.copy()
+    for _ in range(num_iters):
+        J, dw, db = cost_function(x, y, w_opt, b_opt)
+        J_list.append(J)
+        w_opt = w_opt - alpha * dw
+        b_opt = b_opt - alpha * db
+
+    return J_list, w_opt, b_opt
--- neuralNetworks/src/py/logReg/test.py
+++ neuralNetworks/src/py/logReg/test.py
@@ -9,13 +9,16 @@ from logRegCostFctn import *
 def main():
     x = np.random.rand(2, 10)
     y = np.random.randint(2, size=(1, 10))
-    #w = np.random.rand(2, 1)
-    #b = np.random.rand(1)
-
-    w =
+    w = np.random.rand(2, 1)
+    b = np.random.rand(1)
 
     J, dw, db = cost_function(x, y, w, b)
     print(J)
+    pprint(dw)
+    pprint(db)
+
+    J_list, w_opt, b_opt = grad_descent(x, y, w, b, 0.001, 50)
+    pprint(J_list)
     pprint(dw)
     pprint(db)
 
