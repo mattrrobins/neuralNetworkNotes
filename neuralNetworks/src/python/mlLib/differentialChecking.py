f06c73d95c278082ca84d3704cca65d249a40eac
Matt Robinson <mattrrobins@gmail.com>
2022-07-13 16:23:19 -0700

    continuing to work on generalizing the reverse differential checking method to incorporate all possibilities in a single function

--- neuralNetworks/src/python/mlLib/differentialChecking.py
+++ neuralNetworks/src/python/mlLib/differentialChecking.py
@@ -3,4 +3,188 @@
 ## Reverse Differential Checking ##
 
 import numpy as np
+from numpy.linalg import norm
 
+
+def differential_check(f, x, eps=1e-3):
+    """
+    Parameters:
+    -----------
+    f : function
+    x : array_like
+    eps : float
+        Default = 10^{-3}
+
+    Returns:
+    --------
+    error
+    """
+    y, rf = f(x)
+    x = np.array(x)
+    if len(x.shape) == 0:
+        x = x.reshape(1, 1)
+    elif len(x.shape) == 1:
+        x = x.reshape(-1, 1)
+        # if len(y.shape) == 0:
+        # y = y.reshape(1, 1)
+        # elif len(y.shape) == 1:
+        # y = y.reshape(-1, 1)
+
+    # k, l = y.shape
+    m, n = x.shape
+    # F = np.zeros((m, n, k, l))
+    F = np.zeros((m, n, *y.shape))
+    print(F.shape)
+    # rf = rf.reshape(m, n, k, l)
+
+    for i in range(m):
+        for j in range(n):
+            e = np.zeros((m, n))
+            e[i, j] = 1
+            x_plus = x + eps * e
+            x_minus = x - eps * e
+            f_plus, _ = f(x_plus)
+            f_minus, _ = f(x_minus)
+            # f_diff = f_plus.reshape(k, l) - f_minus.reshape(k, l)
+            f_diff = f_plus - f_minus
+            F[i, j] = f_diff
+
+    F = F / (2 * eps)
+
+    error = norm(F - rf) / (norm(F) + norm(rf))
+
+    return error
+
+
+def sigmoid(x):
+    ## sigmoid: ℝ^n → ℝ^n ##
+    # n = 1 is valid
+    x = np.array(x)
+    sigma = 1 / (1 + np.exp(-x))
+
+    dsigma = np.diagflat(sigma * (1 - sigma))
+    rsigma = dsigma.T
+    return sigma, rsigma
+
+
+def foo(x):
+    ## f: ℝ^3 → ℝ^2 ##
+    ## f(x, y, z) = (xy, z^2)
+
+    y = np.zeros((2, 1))
+    y[0] = x[0] * x[1]
+    y[1] = x[2] ** 2
+
+    J = np.zeros((2, 3))
+    J[0, 0] = x[1]
+    J[0, 1] = x[0]
+    J[1, 2] = 2 * x[2]
+
+    R = J.T
+    return y, R
+
+
+def bar(x):
+    ## f: ℝ^{m×n} → ℝ^m ##
+    ## f(x) = x@v
+    np.random.seed(1)
+    m, n = x.shape
+    v = np.random.randn(n)
+    f = np.einsum("ij, j", x, v)
+
+    J = np.zeros((m, m, n))
+    for mu in range(m):
+        for i in range(m):
+            for j in range(n):
+                if mu == i:
+                    J[mu, i, j] = v[j]
+
+    R = np.einsum("kij->ijk", J)
+    return f, R
+
+
+def baz(x):
+    ## f: ℝ^{m×n} → ℝ^{m×n} ##
+    ## f(x) = x * x  # The Hadmard square
+    m, n = x.shape
+    f = np.einsum("ij,ij->ij", x, x)
+
+    J = np.zeros((m, n, m, n))
+    for mu in range(m):
+        for nu in range(n):
+            for i in range(m):
+                for j in range(n):
+                    if (mu == i) and (nu == j):
+                        J[mu, nu, i, j] = 2 * x[i, j]
+
+    R = np.einsum("ijkl->klij", J)
+    return f, R
+
+
+if __name__ == "__main__":
+
+    def differential_check_01(f, x, eps=1e-3):
+        """
+        Parameters:
+        -----------
+        f : function
+        x : array_like
+        eps : float
+            Default = 10^{-3}
+
+        Returns:
+        --------
+        error
+        """
+        y, rf = f(x)
+        x = np.array(x)
+        if len(x.shape) == 0:
+            x = x.reshape(1, 1)
+        elif len(x.shape) == 1:
+            x = x.reshape(-1, 1)
+        if len(y.shape) == 0:
+            y = y.reshape(1, 1)
+        elif len(y.shape) == 1:
+            y = y.reshape(-1, 1)
+
+        k, l = y.shape
+        m, n = x.shape
+        F = np.zeros((m, n, k, l))
+        rf = rf.reshape(m, n, k, l)
+
+        for mu in range(k):
+            for nu in range(l):
+                for i in range(m):
+                    for j in range(n):
+                        e = np.zeros((m, n))
+                        e[i, j] = 1
+                        x_plus = x + eps * e
+                        x_minus = x - eps * e
+                        f_plus, _ = f(x_plus)
+                        f_minus, _ = f(x_minus)
+                        f_diff = f_plus.reshape(k, l) - f_minus.reshape(k, l)
+                        F[i, j, mu, nu] = f_diff[mu, nu]
+
+        F = F / (2 * eps)
+
+        error = norm(F - rf) / (norm(F) + norm(rf))
+
+        return error
+
+    def fctn(x):
+        x = np.array(x)
+        f = np.exp(x)
+        assert f.shape == x.shape
+        return f, np.diagflat(f)
+
+    m = 3
+    n = 4
+    np.random.seed(1)
+    for _ in range(5):
+        x = np.random.rand(m) * 10
+        # x = np.random.randint(1, 100)
+        print(x)
+        # e = differential_check_euclidean(foo, x)
+        # e = differential_check_matrix_to_euclidean(bar, x)
+        e = differential_check(sigmoid, x)
+        print(e)
--- neuralNetworks/src/python/mlLib/test.py
+++ neuralNetworks/src/python/mlLib/test.py
@@ -2,50 +2,56 @@
 
 import numpy as np
 
-class ShuffleBatchData():
-    def __init__(self, data, batch_size, seed=10101):
-        """
-        """
-        self.data = data
-        self.batch_size = batch_size
-        self.seed = seed
-        self.idx = np.arange(data['x'].shape[1])
-        self.__N = data['x'].shape[1]
 
-        np.random.seed(seed)
-    
-    def get_batches(self):
-        """
-        """
-        np.random.shuffle(self.idx)
-        x_shuffled = self.data['x'][:, self.idx]
-        y_shuffled = self.data['y'][:, self.idx]
+def foo_scalar(x):
+    f = x * x
+    df = 2 * x
 
-        B = int(np.ceil(self.__N / self.batch_size))
-
-        batches = []
-        for i in range(B):
-            x_aux = x_shuffled[:, (self.batch_size * i):(self.batch_size * (i + 1))]
-            y_aux = y_shuffled[:, (self.batch_size * i):(self.batch_size * (i + 1))]
-            batches.append({'x' : x_aux, 'y' : y_aux})
-
-        return batches
+    return f, df
 
 
-if __name__ == '__main__':
-    from pprint import pprint as pp
+def foo_vector(x):
+    f = x * x
+    n = x.size
+    df = np.zeros((n, n))
+    for mu in range(n):
+        for i in range(n):
+            if mu == i:
+                df[mu, i] = 2 * x[i]
+
+    return f, df
 
 
-    x = np.arange(1000).reshape(10, 100)
-    y = np.arange(100).reshape(-1, 100)
-    data = {'x' : x, 'y' : y}
+def foo_matrix(x):
+    f = x * x
+    m, n = x.shape
+    df = np.zeros((m, n, m, n))
+    for mu in range(m):
+        for nu in range(n):
+            for i in range(m):
+                for j in range(n):
+                    if (mu == i) and (nu == j):
+                        df[mu, nu, i, j] = 2 * x[i, j]
 
-    batch_size = 2 ** 5
-    batching = ShuffleBatchData(data, batch_size=batch_size)
-    for i in range(10):
-        batches = batching.get_batches()
-        for batch in batches:
-            #pp(batch['x'])
-            pp(batch['y'])
+    return f, df
 
 
+if __name__ == "__main__":
+    np.random.seed(1)
+    for i in range(3):
+        print(f"Iteration {i}")
+
+        x = np.random.randn(1)
+        f, df = foo_scalar(x)
+        print(f)
+        print(df)
+
+        x = np.random.randn(5)
+        f, df = foo_vector(x)
+        print(f)
+        print(df)
+
+        x = np.random.randn(2, 3)
+        f, df = foo_matrix(x)
+        print(f)
+        print(df)
